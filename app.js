de1d9f437f0eee86ccf18924bdf895ef4e176278
neche <chi2064real@gmail.com>
2022-12-26 15:57:18 +0100

    initialize project and add book route

--- app.js
+++ app.js
@@ -2,18 +2,29 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const config = require("./config/config")
 const dbConnect = require("./db/mongodb")
+const bookRoute = require("./routes/books")
 
 const app = express();
+
+//Connect to Mongodb Database
 dbConnect();
 
-
+// Add Middleware
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+//Route to use as the main entry point
+app.use("/api/v1/books", bookRoute)
 
 app.get("/", (req, res)=>{
     res.send("Welcome to Book Store");
 })
-
+// Error handler middleware
+app.use((err, req, res, next) =>{
+    console.log(err)
+    const errorStatus = err.status || 500
+    res.status(errorStatus).send(err.message)
+    next()
+})
 app.listen(config.PORT, ()=> {
     console.log(`server started listering on http://localhost:${config.PORT}`)
 })
--- validator/books.validators.js
+++ validator/books.validators.js
@@ -1,36 +1,47 @@
 const Joi = require('joi');
 
-const bookSchema = Joi.object({
-    title: Joi.string() 
-           .min(5)
-           .max(255)
-           .trim()
-           .required(),
-    shortDescription: Joi.string()
-          .min(5)
-          .max(500)
-          .trim()
-          .optional(),
-    longDescription: Joi.string()
-           .min(50)
-           .optional()
-           .trim()
-})
-//     .with('username', 'birth_year')
-//     .xor('password', 'access_token')
-//     .with('password', 'repeat_password');
+const bookAddSchema = Joi.object({
+    title: Joi.string().min(5).max(255).trim().required(),
+    shortDescription: Joi.string().min(5).max(500).trim().optional(),
+    longDescription: Joi.string().min(50).optional().trim(),
+    year: Joi.number().integer().required().max(2022),
+    isbn: Joi.string().required().min(10).max(14),
+    price: Joi.number().min(0).required(),
 
-// schema.validate({ username: 'abc', birth_year: 1994 });
-// // -> { value: { username: 'abc', birth_year: 1994 } }
+    createdAt: Joi.date().default(Date.now),
+    lastUpdateAt: Joi.date().default(Date.now)
+});
 
-// schema.validate({});
-// // -> { value: {}, error: '"username" is required' }
+const bookUpdateSchema = Joi.object({
+    title: Joi.string().min(5).max(255).trim(),
+    shortDescription: Joi.string().min(5).max(500).trim(),
+    longDescription: Joi.string().min(50).trim(),
+    year: Joi.number().integer().max(2022),
+    isbn: Joi.string().min(10).max(14),
+    price: Joi.number().min(0),
+});
 
-// // Also -
+async function AddBookValidationMV(req, res, next){
+       const bookPayLoad = req.body;
+       try {
+          await bookAddSchema.validateAsync(bookPayLoad)
+          next()    
+       } catch (error) {
+             next({ message: error.details[0].message , status: 400}); 
+       }
+}
 
-// try {
-//     const value = await schema.validateAsync({
-//         username: 'abc',
-//         birth_year: 1994
-//     });
-// } catch (err) {}
+async function UpdatedBookValidationMV(req, res, next) {
+    const bookPayLoad = req.body;
+    try {
+        await bookUpdateSchema.validateAsync(bookPayLoad);
+        next();
+    } catch (error) {
+        next({ message: error.details[0].message, status: 400 });
+    }
+}
+
+module.exports = {
+    AddBookValidationMV,
+    UpdatedBookValidationMV
+};
